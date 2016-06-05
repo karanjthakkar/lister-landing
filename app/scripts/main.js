ef337a8552f7d84cebf71935ae7c6912c3df5979
Karan Thakkar <karanjthakkar@gmail.com>
2016-06-06 01:38:05 +0530

    Base landing page

---
+++ app/scripts/main.js
@@ -0,0 +1,19 @@
+$('.flexslider').flexslider({
+  // animation: 'slide',
+  // direction: 'horizontal',
+  // reverse: false,
+  // animationLoop: true,
+  // smoothHeight: false,
+  // startAt: 0,
+  // slideshow: true,
+  controlNav: false,
+  directionNav: false,
+  animation: 'slide', // Change Animation Type to fade
+  animationLoop: true,
+  touch: true,
+  directionNav: false,
+  slideshowSpeed: 5000, // Slide Intervals
+  animationSpeed: 600, // Animation Speeds/times
+  slideshow: true,
+  pauseOnAction: false,
+});
