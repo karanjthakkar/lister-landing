4f707ab575b6b725c6bc1176d8bf60f9bb73c8ec
Karan Thakkar <karanjthakkar@gmail.com>
2016-06-06 01:47:37 +0530

    Update animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,13 @@
 $('.flexslider').flexslider({
-  // animation: 'slide',
-  // direction: 'horizontal',
-  // reverse: false,
-  // animationLoop: true,
-  // smoothHeight: false,
-  // startAt: 0,
-  // slideshow: true,
+  animation: 'slide',
+  direction: 'horizontal',
+  reverse: false,
+  animationLoop: true,
+  smoothHeight: false,
+  startAt: 0,
+  slideshow: true,
   controlNav: false,
   directionNav: false,
-  animation: 'slide', // Change Animation Type to fade
-  animationLoop: true,
-  touch: true,
-  directionNav: false,
-  slideshowSpeed: 5000, // Slide Intervals
-  animationSpeed: 600, // Animation Speeds/times
-  slideshow: true,
-  pauseOnAction: false,
+  slideshowSpeed: 4000,
+  animationSpeed: 300
 });
