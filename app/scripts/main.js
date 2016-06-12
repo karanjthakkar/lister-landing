4efdf5e124cda167ff4b865de067a3174aae7609
Karan Thakkar <karanjthakkar@gmail.com>
2016-06-12 23:30:47 +0530

    Add twitter card info and download button analytics

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,3 +11,14 @@ $('.flexslider').flexslider({
   slideshowSpeed: 4000,
   animationSpeed: 300
 });
+
+$('#js-ios-download').on('click', function(event) {
+  event.preventDefault();
+  ga('send', 'event', {
+    eventCategory: 'Download',
+    eventAction: 'iOS'
+  });
+  setTimeout(function() {
+    window.location = 'https://itunes.apple.com/us/app/tweetify/id1122578013?ls=1&mt=8';
+  }, 500);
+});
